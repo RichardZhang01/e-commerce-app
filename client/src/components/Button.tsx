cf3aaccf442a5d8fd7d4a19f1b4443abb11cc28d
Richard Zhang <richardzhiyuanzhang@gmail.com>
2024-08-19 15:04:58 -0400

    adjust button styles

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 interface ButtonProps {
-  onClickHandler: React.MouseEventHandler<HTMLButtonElement>;
+  onClickHandler: () => void;
   buttonText: string;
 }
 
 export const Button = ({ onClickHandler, buttonText }: ButtonProps) => (
   <button
-    className="bg-cyan-500 hover:bg-cyan-600 text-white px-4 py-2 rounded"
+    className="bg-cyan-600 hover:bg-cyan-500 text-white font-semibold px-6 py-2 rounded shadow transition duration-200"
     onClick={onClickHandler}
   >
     {buttonText}
